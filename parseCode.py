c21188212d8499f95b524d88c053d7414c4019e8
Quiana Stodder <qstodder@gmail.com>
2020-09-23 12:56:45 -0700

    venmo function update

--- parseCode.py
+++ parseCode.py
@@ -4,50 +4,47 @@ import os
 
 statements = r'/Users/quiana/Documents/PersonalFinances/accounting 2.0/statements'
 
-incomeKey = ["pasqual", "ucsd apach", "ucsd payrl", "asml", "interest", "deposit", "university of ca"]
-groceriesKey = ["trader joe's", "ralphs", "vons", "myprotein", "safeway"]
-housingKey =   ["premiere", "canyon park", "time warner", "sd gas", "wifi", "state farm"]
-gasKey = ["rotten robbie", "7-eleven", "raley's", "valero", "chevron", "arco", "shell", "stars & stripes", "fort independence", " 76 ", "united pacifi",\
-     "circle k", "texaco", "gas"]
-necesitiesKey = ["bookstore", "walmart", "toyota", "cvs", "cyclery", "regents of uc", "parking", "health", "foothill", "postal", "bird app", \
-    "irs treas", "franchise tax", "calibercollision", "doctor", "best buy", "lyft", "home depot", "automotive"]
-adventuresKey = ["rei", "mesa", "recreation", "ikon", "backcountry", "steepandcheap", "play it again", "airlines", "sportinggoods", "car rental", "best western"]
-funFoodKey = ["fairbanks", "hdh", "bombay coast", "restaurants", "tajima", "primos", "chipotle", "poki", "rubio's", "ramen", "taco", "ballast point", \
-    "rock bottom", "mammoth mtn food", "pete's coffee", "5guys", "mexican", "eatery", "shogun", "pizzeria", "art of espress", "starbucks", "subway"]
-giftsKey = ["gofndme", "wpy", "jacquie lawson", "uncommongoods", "happy earth", "bernie", "4 ocean", "etsy"]
-shoppingKey = ["marshalls", "ross", "nordstrom", "wearlively", "dsw", "shopping", "forever 21"]
-entertainmentKey = ["cinemas", "reel rock", "prime video", "spotify"]
-otherKey = ["schwab"]
+def keywords():
+    incomeKey = ["pasqual", "ucsd apach", "ucsd payrl", "asml", "interest", "deposit", "university of ca"]
+    groceriesKey = ["trader joe's", "ralphs", "vons", "myprotein", "safeway", "food", "milk"]
+    housingKey =   ["premiere", "canyon park", "time warner", "sd gas", "wifi", "state farm", "üè°", "üîå"]   #house
+    gasKey = ["rotten robbie", "7-eleven", "raley's", "valero", "chevron", "arco", "shell", "stars & stripes", "fort independence", " 76 ", "united pacifi",\
+        "circle k", "texaco", "gas"]
+    necesitiesKey = ["bookstore", "walmart", "toyota", "cvs", "cyclery", "regents of uc", "parking", "health", "foothill", "postal", "bird app", \
+        "irs treas", "franchise tax", "calibercollision", "doctor", "best buy", "lyft", "home depot", "automotive"]
+    adventuresKey = ["rei", "mesa", "recreation", "ikon", "backcountry", "steepandcheap", "play it again", "airlines", "sportinggoods", "car rental", "best western"]
+    funFoodKey = ["fairbanks", "hdh", "bombay coast", "restaurants", "tajima", "primos", "chipotle", "poki", "rubio's", "ramen", "taco", "ballast point", \
+        "rock bottom", "mammoth mtn food", "pete's coffee", "5guys", "mexican", "eatery", "shogun", "pizzeria", "art of espress", "starbucks", "subway", \
+            "thai", "ramen", "creps", "üåØ"] #burrito
+    giftsKey = ["gofndme", "wpy", "jacquie lawson", "uncommongoods", "happy earth", "bernie", "4 ocean", "etsy"]
+    shoppingKey = ["marshalls", "ross", "nordstrom", "wearlively", "dsw", "shopping", "forever 21"]
+    entertainmentKey = ["cinemas", "reel rock", "prime video", "spotify"]
+    otherKey = ["schwab"]
 
-keys = [incomeKey, groceriesKey, housingKey, gasKey, necesitiesKey, adventuresKey, funFoodKey, giftsKey, shoppingKey, entertainmentKey, otherKey]
+    return keys = [incomeKey, groceriesKey, housingKey, gasKey, necesitiesKey, adventuresKey, funFoodKey, giftsKey, shoppingKey, entertainmentKey, otherKey]
 
+keys = keywords()
 
 # venmo sorter
 def venmo(file):
-    df = pd.read_csv(statements+"/"+file, header=None)
-    df = df[::-1]
-    df[4] = df[4].str.lower()
+    df = pd.read_csv(statements+"/"+file)
+    df = df[pd.notnull(df["Note"])]
+    df["Note"] = df["Note"].str.lower()
     outTable = pd.DataFrame(None,index=range(len(df.index)), columns=['Date', 'Source', 'Description', 'Income', 'Groceries', 'Housing', 'Gas', 'Necesities', 'Adventures', 'Fun_Food', 'Gifts/Charity', 'Shopping', 'Entertainment', 'Other', 'Unknown', 'Details'])
-    skip=["venmo", "discover e-payment", "paypal", "online transfer", "wf credit card"]
     for iRow, row in df.iterrows():
         found = False
-        # print(row[4])
-        outTable['Date'][iRow] = row[0]
-        outTable['Source'][iRow] = 'debit'
-        outTable['Details'][iRow] = row[4]
+        outTable['Date'][iRow] = row["Datetime"]
+        outTable['Source'][iRow] = 'venmo'
+        outTable['Details'][iRow] = row["Note"]
         for c in range(len(keys)):
             for k, key in enumerate(keys[c]):
-                if key in row[4]:
+                if key in row["Note"]:
                     outTable['Description'][iRow] = key
-                    outTable.iloc[iRow, c+3] = row[1]
+                    outTable.iloc[iRow, c+3] = row["Amount (total)"]
                     found = True
         if not found:
-            outTable['Unknown'][iRow] = row[1]
-        for x in skip:
-            if x in row[4]:
-                outTable['Date'][iRow] = 0
+            outTable['Unknown'][iRow] = row["Amount (total)"]
 
-    outTable = outTable[outTable.Date != 0]
     return outTable
     
 # debit sorter
@@ -156,6 +153,7 @@ def addTable(finalTable, addition):
         finalTable = finalTable.append(addition, ignore_index=True)
     return finalTable
 
+
 # read all files in statements folder
 files = [x[2] for x in os.walk(statements)]
 finalTable = pd.DataFrame()
@@ -169,6 +167,9 @@ for f, file in enumerate(files[0]):
     elif "discover" in file:
         discData = discover(file)
         finalTable = addTable(finalTable, discData)
+    elif "venmo" in file:
+        venmoData = venmo(file)
+        finalTable = addTable(finalTable, venmoData)
 
 print(finalTable.head())
 finalTable.to_csv("allStatements.csv")
