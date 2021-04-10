0eca542e39375369c0a4dd438d7528bd8efa12bd
Quiana Stodder <qstodder@gmail.com>
2021-04-10 13:02:33 -0700

    updated venmo funciton to accomodate new csv format

--- parseCode.py
+++ parseCode.py
@@ -8,7 +8,7 @@ import datetime
 
 statements = r'/Users/quiana/Documents/PersonalFinances/accounting/newStatements'
 oldStatements = r'/Users/quiana/Documents/PersonalFinances/accounting/oldStatements'
-allStatements = r'/Users/quiana/Documents/PersonalFinances/accounting/allStatements.csv'
+allStatements = r'/Users/quiana/Documents/PersonalFinances/accounting/personalAccounting/allStatements.csv'
 
 def addTable(finalTable, addition):
     if finalTable.size == 0:
@@ -40,7 +40,7 @@ def listEmojis(text):
     return emoji_list
 
 def keywords():
-    incomeKey = ["pasqual", "ucsd apach", "ucsd payrl", "asml", "interest", "deposit", "university of ca", "payroll"]
+    incomeKey = ["pasqual", "ucsd apach", "ucsd payrl", "asml", "interest", "deposit", "university of ca", "payroll", "uc san diego ucsd"]
     groceriesKey = ["trader joe", "ralph", "vons", "myprotein", "safeway", "food", "milk", "groceries", "fud", "tj's", "snax", "🥒🍅"]
     housingKey =   ["premiere", "canyon park", "time warner", "sd gas", "sewwer", "rent", "insurance", "wifi", "state farm", "util", "sdge", "🏠", "🚿", "🏡", "💡", "🔌"]  
     gasKey = ["rotten robbie", "7-eleven", "raley's", "valero", "chevron", "arco", "shell", "stars & stripes", "fort independence", " 76 ", "united pacifi",\
@@ -52,7 +52,7 @@ def keywords():
         "rock bottom", "mammoth mtn food", "pete's coffee", "5guys", "mexican", "eatery", "shogun", "pizzeria", "art of espress", "starbucks", "subway", \
             "thai", "ramen", "creps", "pizza", "yums", "fooood", "lunch","acaii", "chicago 🔥 grill", "😋", "🌯", "🍔", "🍟", "🍺", "🍴", "🍕", "🍹", "🍦", "🍻", "🍣"]
     giftsKey = ["gofndme", "wpy", "jacquie lawson", "uncommongoods", "happy earth", "bernie", "4 ocean", "etsy"]
-    shoppingKey = ["marshalls", "ross", "nordstrom", "wearlively", "dsw", "shopping", "forever 21"]
+    shoppingKey = ["marshalls", "ross", "nordstrom", "wearlively", "dsw", "shopping", "forever 21", "poshmark", "lulus"]
     entertainmentKey = ["cinemas", "reel rock", "prime video", "spotify", "movie", "web town square", "🎥"]
     otherKey = ["schwab"]
 
@@ -69,23 +69,22 @@ def debit(file):
     for iRow, row in df.iterrows():
         found = False
         count = 0
-        # print(row[4])
-        outTable['Date'][iRow] = toDatetime(row[0])
-        outTable['Source'][iRow] = 'debit'
-        outTable['Details'][iRow] = row[4]
+        outTable.loc[iRow, 'Date'] = toDatetime(row[0])
+        outTable.loc[iRow, 'Details'] = row[4]
+        outTable.loc[iRow, 'Source'] = 'debit'
         for c in range(len(keys)):
             for k, key in enumerate(keys[c]):
                 if key in row[4]:
-                    outTable['Description'][iRow] = key
+                    outTable.loc[iRow, 'Description'] = key
                     outTable.iloc[iRow, c+3] = row[1]
                     found = True
                     count += 1
         if not found:
-            outTable['Unknown'][iRow] = row[1]
+            outTable.loc[iRow, 'Unknown'] = row[1]
             count += 1
         for x in skip:
             if x in row[4]:
-                outTable['Date'][iRow] = 0
+                outTable.loc[iRow, 'Date'] = 0
         outTable['NumCategories'] = count
 
     outTable = outTable[outTable.Date != 0]
@@ -100,22 +99,22 @@ def credit(file):
     for iRow, row in df.iterrows():
         found = False
         count = 0
-        outTable['Date'][iRow] = toDatetime(row[0])
-        outTable['Source'][iRow] = 'credit'
-        outTable['Details'][iRow] = row[4]
+        outTable.loc[iRow, 'Date'] = toDatetime(row[0])
+        outTable.loc[iRow, 'Source'] = 'credit'
+        outTable.loc[iRow, 'Details'] = row[4]
         for c in range(len(keys)):
             for k, key in enumerate(keys[c]):
                 if key in row[4]:
-                    outTable['Description'][iRow] = key
+                    outTable.loc[iRow, 'Description'] = key
                     outTable.iloc[iRow, c+3] = row[1]
                     found = True
                     count += 1
         if not found:
-            outTable['Unknown'][iRow] = row[1]
+            outTable.loc[iRow, 'Unknown'] = row[1]
             count += 1
         for x in skip:
             if x in row[4]:
-                outTable['Date'][iRow] = 0
+                outTable.loc[iRow, 'Date'] = 0
         outTable['NumCategories'] = count
 
     outTable = outTable[outTable.Date != 0]
@@ -131,45 +130,45 @@ def discover(file):
     for iRow, row in df.iterrows():
         found = False
         count = 0
-        outTable['Date'][iRow] = toDatetime(row["Trans. Date"])
-        outTable['Source'][iRow] = 'discover'
-        outTable['Details'][iRow] = row["Description"]
+        outTable.loc[iRow, 'Date'] = toDatetime(row["Trans. Date"])
+        outTable.loc[iRow, 'Source'] = 'discover'
+        outTable.loc[iRow, 'Details'] = row["Description"]
         for c in range(len(keys)):
             for k, key in enumerate(keys[c]):
                 if key in row["Description"]:
-                    outTable['Description'][iRow] = key
+                    outTable.loc[iRow, 'Description'] = key
                     outTable.iloc[iRow, c+3] = row["Amount"]
                     found = True
                     count += 1
         if not found:
             if row[4] == "Supermarkets":
-                outTable['Description'][iRow] = "Supermarket"
-                outTable['Groceries'][iRow] = row["Amount"]
+                outTable.loc[iRow, 'Description'] = "Supermarket"
+                outTable.loc[iRow, 'Groceries'] = row["Amount"]
             elif row[4] == "Restaurants":
-                outTable['Description'][iRow] = "Restaurant"
-                outTable['Fun_Food'][iRow] = row["Amount"]
+                outTable.loc[iRow, 'Description'] = "Restaurant"
+                outTable.loc[iRow, 'Fun_Food'] = row["Amount"]
             elif row[4] == "Government Services":
-                outTable['Description'][iRow] = "Gov. Service"
-                outTable['Necesities'][iRow] = row["Amount"]
+                outTable.loc[iRow, 'Description'] = "Gov. Service"
+                outTable.loc[iRow, 'Necesities'] = row["Amount"]
             elif row[4] == "Department Stores":
-                outTable['Description'][iRow] = "Department Store"
-                outTable['Shopping'][iRow] = row["Amount"]
+                outTable.loc[iRow, 'Description'] = "Department Store"
+                outTable.loc[iRow, 'Shopping'] = row["Amount"]
             elif row[4] == "Gasoline":
-                outTable['Description'][iRow] = "gas"
-                outTable['Gas'][iRow] = row["Amount"]
+                outTable.loc[iRow, 'Description'] = "gas"
+                outTable.loc[iRow, 'Gas'] = row["Amount"]
             else:
-                outTable['Unknown'][iRow] = row["Amount"]
+                outTable.loc[iRow, "Unknown"] = row["Amount"]
             count += 1
         for x in skip:
             if x in row["Description"]:
-                outTable['Date'][iRow] = 0
+                outTable.loc[iRow, 'Date'] = 0
         outTable['NumCategories'] = count
 
     outTable = outTable[outTable.Date != 0]
     return outTable
 
 def venmo(file):
-    df = pd.read_csv(statements+"/"+file)
+    df = pd.read_csv(statements+"/"+file, header=2)
     df = df[pd.notnull(df["Note"])]
     df = df.reset_index(drop=True)
     df["Note"] = df["Note"].str.lower()
@@ -177,22 +176,23 @@ def venmo(file):
     for iRow, row in df.iterrows():
         found = False
         count = 0
-        outTable['Date'][iRow] = toDatetime(row["Datetime"], isStandard=False)
-        outTable['Source'][iRow] = 'venmo'
-        outTable['Details'][iRow] = row["Note"]
+        outTable.loc[iRow, 'Date'] = toDatetime(row["Datetime"], isStandard=False)
+        outTable.loc[iRow, 'Source'] = 'venmo'
+        outTable.loc[iRow, 'Details'] = row["Note"]
         amount = row['Amount (total)']
-        amount = float(amount.replace(" $",""))
+        amount = amount.replace(" ","").replace("(","-").replace(")","").replace(",","")
+        amount = float(amount.replace("$",""))
         for c in range(len(keys)):
             for k, key in enumerate(keys[c]):
                 if key in row["Note"]:
-                    outTable['Description'][iRow] = key
+                    outTable.loc[iRow, 'Description'] = key
                     outTable.iloc[iRow, c+3] = amount
                     found = True
                     count += 1
         if not found:
             count += 1
-            outTable['Unknown'][iRow] = amount
-            outTable['Details'][iRow] = row["Note"] + (' '.join(thing for thing in [' ', row["From"], row["Type"], row["To"]]))
+            outTable.loc[iRow, 'Unknown'] = amount
+            outTable.loc[iRow, 'Details'] = row["Note"] + (' '.join(thing for thing in [' ', row["From"], row["Type"], row["To"]]))
             if listEmojis(row["Note"]):
                 emojis = (' '.join(emoji for emoji in listEmojis(row["Note"])))
                 print(row["Note"] + emojis)
@@ -218,16 +218,19 @@ for f, file in enumerate(files[0]):
         venmoData = venmo(file)
         finalTable = addTable(finalTable, venmoData)
 
+# move new statements to old statements folder
 for f, file in enumerate(files[0]):
     shutil.move(statements+"/"+file, oldStatements+"/"+file)
 
-print(finalTable.head())
+print(finalTable.tail())
 filename = "allStatements.csv" 
 finalTable.to_csv(filename)
 
 # create analysis: monthly summation
 dates = pd.to_datetime(finalTable['Date']).sort_values()
 months = dates.dt.strftime("%m/%y").unique().tolist()
+cleanedMonths = [x for x in months if str(x) != 'nan']
+months = cleanedMonths
 months_dt = [datetime.datetime(int("20" + d.split('/')[1]), int(d.split('/')[0]), 1) for d in months]
 
 aggMonths = pd.DataFrame(None,index=range(len(months)), columns=['Month', 'Income', 'Groceries', 'Housing', 'Gas', 'Necesities', 'Adventures', 'Fun_Food', 'Gifts/Charity', 'Shopping', 'Entertainment', 'Other', 'Unknown'])
@@ -250,7 +253,7 @@ aggMonths['AvgGroceries'] = [round(num,2) for num in aggMonths['Groceries']/4.5]
 aggMonths['AvgFood'] = [round(num,2) for num in (aggMonths['Groceries'] + aggMonths['Fun_Food'])/4.5]
 
 
-print(aggMonths.head())
+print(aggMonths.tail())
 # print(months_dt)
 
 
